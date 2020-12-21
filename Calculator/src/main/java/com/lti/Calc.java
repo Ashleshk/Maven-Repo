9cfb427b3a542ffcdf2d62f442f6fe27df3e5432
Ashleshk <ashleshuk@gmail.com>
2020-12-21 15:00:39 +0530

    Division Method Added

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -10,8 +10,13 @@ public class Calc {
 	public static int mul(int a, int b){
 		return a*b;
 	}
+	public static int div(int a, int b){
+		return a/b;
+	}
 	public static void main(String[] args) {
 		System.out.println("Additon: "+ add(12,14));
 		System.out.println("Subtraction: "+ sub(12,10));
+		System.out.println("Multiply: "+ mul(2,14));
+		System.out.println("Division: "+ div(12,4));
 	}
 }
