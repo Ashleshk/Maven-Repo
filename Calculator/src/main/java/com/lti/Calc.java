b618e127a2a504849f71f8cc0267af33ea24f422
Ashleshk <ashleshuk@gmail.com>
2020-12-21 12:43:34 +0530

    Calculator Class Created

---
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -0,0 +1,10 @@
+package com.lti;
+
+public class Calc {
+	public static int add(int a, int b){
+		return a+b;
+	}
+	public static void main(String[] args) {
+		System.out.println("Additon: "+ add(12,14));
+	}
+}
