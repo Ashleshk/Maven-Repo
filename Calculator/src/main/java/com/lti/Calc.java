29d2d9c81c7881b6ea5096976c7744d80444c6e5
Ashleshk <ashleshuk@gmail.com>
2020-12-21 14:59:34 +0530

    Multiplication Method

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -7,6 +7,9 @@ public class Calc {
 	public static int sub(int a, int b){
 		return a-b;
 	}
+	public static int mul(int a, int b){
+		return a*b;
+	}
 	public static void main(String[] args) {
 		System.out.println("Additon: "+ add(12,14));
 		System.out.println("Subtraction: "+ sub(12,10));
