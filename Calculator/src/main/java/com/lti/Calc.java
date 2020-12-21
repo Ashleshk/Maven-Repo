7e87915c5b6c248176b82f723b3d820cc3ba6069
Ashleshk <ashleshuk@gmail.com>
2020-12-21 15:15:56 +0530

    ssqaure method

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -16,11 +16,15 @@ public class Calc {
 	public static int mod(int a, int b){
 		return a%b;
 	}
+	public static int square(int a){
+		return a*a;
+	}
 	public static void main(String[] args) {
 		System.out.println("Additon: "+ add(12,14));
 		System.out.println("Subtraction: "+ sub(12,10));
 		System.out.println("Multiply: "+ mul(2,14));
 		System.out.println("Division: "+ div(12,4));
 		System.out.println("Modulus: "+ mod(12,4));
+		System.out.println("Sqaure: "+ square(12));
 	}
 }
