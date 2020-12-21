93e36adfc8556ff489de4e42a77ccecf19c989e9
Ashleshk <ashleshuk@gmail.com>
2020-12-21 14:58:23 +0530

    Added Another Method

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -4,7 +4,11 @@ public class Calc {
 	public static int add(int a, int b){
 		return a+b;
 	}
+	public static int sub(int a, int b){
+		return a-b;
+	}
 	public static void main(String[] args) {
 		System.out.println("Additon: "+ add(12,14));
+		System.out.println("Subtraction: "+ sub(12,10));
 	}
 }
