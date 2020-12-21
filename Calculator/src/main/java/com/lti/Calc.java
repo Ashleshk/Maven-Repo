9ac66aafba107714c1907f05ba3b34c563575981
Ashleshk <ashleshuk@gmail.com>
2020-12-21 15:08:53 +0530

    modulus

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -13,10 +13,14 @@ public class Calc {
 	public static int div(int a, int b){
 		return a/b;
 	}
+	public static int mod(int a, int b){
+		return a/b;
+	}
 	public static void main(String[] args) {
 		System.out.println("Additon: "+ add(12,14));
 		System.out.println("Subtraction: "+ sub(12,10));
 		System.out.println("Multiply: "+ mul(2,14));
 		System.out.println("Division: "+ div(12,4));
+		System.out.println("Modulus: "+ mod(12,4));
 	}
 }
